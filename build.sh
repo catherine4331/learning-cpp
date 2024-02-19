17632aa667de3ccb001102f07eedbba437e2eefb
catherine4331 <catherine4331@gmail.com>
2024-02-19 18:34:00 +0000

    Hello, world!

---
+++ build.sh
@@ -0,0 +1,3 @@
+# At some point I will set up something like CMake but right now, this will do
+
+g++-13.2.0 -std=c++2b -fmodules-ts -Wall -o app main.cpp
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#if __INTELLISENSE__
+#include <iostream>
+#else
+import <iostream>;
+#endif
+
+int main()
+{
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
