5d24520ce54fcad351360d84b0e47ccb28e8a264
catherine4331 <catherine4331@gmail.com>
2024-02-19 21:02:06 +0000

    Our first module

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 # At some point I will set up something like CMake but right now, this will do
 
+g++-13.2.0 -std=c++2b -fmodules-ts -c -x c++ employee.cppm
+
 g++-13.2.0 -std=c++2b -fmodules-ts -Wall -o app main.cpp
--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #if __INTELLISENSE__
 #include <iostream>
+#include <limits>
 #else
 import <iostream>;
+import <limits>;
 #endif
+
+import employee;
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    cout << "There are " << 8 << " million ways to die!" << endl;
+    cout << "The max int is " << numeric_limits<int>::max() << endl;
+
+    Employee anEmployee;
+    anEmployee.firstInitial = 'J';
+    anEmployee.lastInitial = 'D';
+    anEmployee.employeeNumber = 42;
+    anEmployee.salary = 80000;
+
+    cout << "Employee: " << anEmployee.firstInitial << anEmployee.lastInitial << endl;
+    cout << "Number: " << anEmployee.employeeNumber << endl;
+    cout << "Salary: $" << anEmployee.salary << endl;
+
     return 0;
 }
